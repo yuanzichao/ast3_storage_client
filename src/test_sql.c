394f02c8bb6950c5d21851650d3e88d85f4e22e2
chao <1162039386@qq.com>
2015-05-17 09:29:24 +0800

    新增测试插入磁盘信息

--- src/test_sql.c
+++ src/test_sql.c
@@ -9,14 +9,29 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <time.h>
+
+#include "type_def.h"
 #include "sql.h"
 
 
 int main(void) {
 
-    Disk_Info disk_info;
+	init_mysql();          //初始化数据库连接
 
-	init_mysql();
+	//获取系统当前时间并格式化
+	time_t calendar_time = time(NULL);
+	struct tm * tm_local = localtime(&calendar_time);
+	char str_f_t [LEN];
+	strftime(str_f_t, sizeof(str_f_t), "%G-%m-%d %H:%M:%S", tm_local);
+
+
+    Disk_Info disk_info = {0, "disk_003", "1cbb5d04d65742e0ac6a532167a61262", "ext3/ext4", 1024.00, 300.00, str_f_t, 0, 0};
+    insert_disk(disk_info);    //测试插入磁盘信息
+
+
+
+
     query_disks();        //测试磁盘查询
     print_result();
     free_result();
