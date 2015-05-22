de86e46b45e704fdd4ff449be8a328d101c7b773
chao <1162039386@qq.com>
2015-05-22 20:32:10 +0800

    磁盘操作函数测试(已全部测试通过)

--- src/test_sql.c
+++ src/test_sql.c
@@ -16,59 +16,58 @@
 
 int main(void) {
 
-	init_mysql();          //初始化数据库连接
+	//函数接口测试
 
-//	//获取系统当前时间并格式化
-//	time_t calendar_time = time(NULL);
-//	struct tm * tm_local = localtime(&calendar_time);
-//	char str_f_t [LEN];
-//	strftime(str_f_t, sizeof(str_f_t), "%G-%m-%d %H:%M:%S", tm_local);
-
-//	//初始化Disk_Info
-//    Disk_Info disk_info = {0, "disk_005", "5a9068d5a7bf4b3bbec918af44e9d4e0", "ext3/ext4", 1024.00, 300.00, "", 0, 0};
-//    sprintf(disk_info.recent_use_time, "%s", str_f_t);  //时间单独赋值
-//
-//    insert_disk(disk_info);    //测试插入磁盘信息
-
-//    Directory_Info dir_info = {0, "2015051", "5a9068d5a7bf4b3bbec918af44e9d4e0", "disk_005", 0, 800, "", "", 0, 0};
-//    sprintf(dir_info.recent_use_time, "%s", str_f_t);  //时间单独赋值
-//    sprintf(dir_info.time, "%s", str_f_t);  //时间单独赋值
-//
-//    insert_directory(dir_info);  //测试插入目录信息
-
-//    //同时更新Disk_Info
-//    Disk_Info disk_info = {};
-//	query_disk("disk_005");
-//
-//    disk_info = get_disk_info(mysql_fetch_row(g_res));
-//
-//    printf("%s\n", disk_info.disk_name);
-//    update_disk(disk_info);
+	init_mysql();		//初始化数据库连接
 
 
+	/***************************************************************************/
+	/************************* 磁盘操作函数测试 **********************************/
+	/***************************************************************************/
+	printf("测试：查询所有硬盘信息\n");
+	query_disks();		//查询所有硬盘信息
+	print_result();
+	free_result();
 
+	printf("测试：获取磁盘ID\n");
+	char *disk_id;
+	disk_id = get_disk_id("disk_002");		//获取磁盘ID
+	printf("disk_id==>>%s\n", disk_id);
+	free_result();
 
+	printf("测试：查询指定硬盘信息\n");
+	query_disk("disk_002");		//查询指定硬盘信息
+	print_result();
+	free_result();
 
-    query_disks();        //测试磁盘查询
-    print_result();
-    free_result();
+	printf("测试：获取db_disk_info\n");
+	db_disk_info *disk_info;
+	disk_info = db_disk_info__new();
+	disk_info = get_disk_info("disk_002");		//获取db_disk_info
+	printf("disk_id==>>%d && disk_name==>>%s\n", disk_info->disk_id, disk_info->disk_name);
 
-    query_disk("disk_001");         //测试查询指定磁盘
-    print_result();
-    free_result();
+	printf("测试：插入磁盘信息\n");
+	db_disk_info *disk_info_01;
+	disk_info_01 = db_disk_info__new();
+	disk_info_01->disk_name = "disk_insert_test";
+	disk_info_01->disk_uuid = "51bb74393f2646d7bd46b4427ade5e17";
+	disk_info_01->disk_type = "ext3/ext4";
+	disk_info_01->disk_capacity = 700.1;
+	disk_info_01->disk_used = 300;
+	time_t cur_time = time(NULL);		//获取系统当前时间并格式化
+	disk_info_01->recent_use_time = cur_time;
+	disk_info_01->disk_status = 0;
+	disk_info_01->permisssion = 0;
+	insert_disk(disk_info_01);			//插入磁盘信息
 
+	printf("测试：更新磁盘信息\n");
+	disk_info->disk_used = 100;		//使用上一步得到的db_disk_info
+	update_disk(disk_info);			//更新磁盘信息
 
-    query_disks_info("disk_001");         //测试磁盘内目录查询
-    print_result();
-    free_result();
-
-    query_directory_info("20150424", "disk_001");     //测试目录内文件查询信息
-    print_result();
-    free_result();
-
-    query_file_info("20150424_0001.fits", "20150424", "disk_001");     //测试文件查询信息
-    print_result();
-    free_result();
+	printf("测试：查询硬盘内目录信息\n");
+	query_disks_info("disk_002");			//查询硬盘内目录信息
+	print_result();
+	free_result();
 
 
     close_db(); // 关闭链接
